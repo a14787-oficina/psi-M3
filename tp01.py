b363c069f07a2b59bfb20782480b858549efbf0d
a14787-oficina <a14787@oficina.pt>
2026-04-23 10:06:50 +0100

    Delete tp01.py

--- tp01.py
+++
@@ -1,9 +0,0 @@
-saldo = 100
-valor_levantar = float(input("Qual é o valor que quer levantar? "))
-restante = saldo - valor_levantar
-
-if valor_levantar <= saldo:
-    print("Levantamento autorizado")
-    print(f"Levantaste {valor_levantar} e o saldo restante é {restante}")
-else:
-    print(f"Saldo insuficiente, ficas com um saldo negativo de {restante}")
