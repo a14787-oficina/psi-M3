d73e30a729da8c45ba729b9c0c1e8bd184f72313
a14787-oficina <a14787@oficina.pt>
2026-04-23 10:06:35 +0100

    Delete exerc-03.py

--- exerc-03.py
+++
@@ -1,7 +0,0 @@
-user = str(input("Insira o username: "))
-password = int(input("Digite a palavra passe: "))
-
-user_correto = "admin"
-passe_correta = 1234
-
-print(user == user_correto and password == passe_correta)
